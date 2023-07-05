d736c130a90c9bce894dd5be4c7f1920aa8085f5
Vaibhav24y <118295704+Vaibhav24y@users.noreply.github.com>
2023-07-05 14:53:48 +0530

    Add files via upload

---
+++ assigment9.py
@@ -0,0 +1,14 @@
+from calc2 import Calculator
+import calculate as m
+print("Math Operations:")
+print(MathOperations.add(7,3))
+print(MathOperations.subtract(8,3))
+print(MathOperations.multiply(4,5))
+print(MathOperations.divide(15, 2))
+a=5.1
+print(m.ceil(a))
+b=5.99
+print(m.floor(b))
+print(m.sin(60))
+print(m.fsum([1,2,3,4,5,6,7,8]))
+print(m.pow(4,5))
